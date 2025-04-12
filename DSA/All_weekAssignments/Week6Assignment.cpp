1945fd8e0b1027d82e1c6a1f1e8d35fa1839f169
Himanshujaglyan <114854175+Himanshujaglyan@users.noreply.github.com>
2025-04-11 20:47:08 -0700

    Pointer class 1

--- DSA/All_weekAssignments/Week6Assignment.cpp
+++ DSA/All_weekAssignments/Week6Assignment.cpp
@@ -1,5 +1,15 @@
+//!Pointers
+
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+    int  a = 3;
+    // cout<<&a<<endl;
+    int* ptr = &a;
+    // cout<<&ptr;
+    *ptr = *ptr+2;
+    // cout<<a;
+    cout<<sizeof(ptr);
+
     return 0;
 }
