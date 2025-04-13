96f2b082effdc85db8acb1e127883091d127ebce
Himanshujaglyan <114854175+Himanshujaglyan@users.noreply.github.com>
2025-04-12 23:51:52 -0700

    Pointers in Array

--- DSA/All_weekAssignments/Week6Assignment.cpp
+++ DSA/All_weekAssignments/Week6Assignment.cpp
@@ -3,13 +3,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int  a = 3;
+    // int  a = 3;
     // cout<<&a<<endl;
-    int* ptr = &a;
+    // int* ptr = &a;
     // cout<<&ptr;
-    *ptr = *ptr+2;
+    // *ptr = *ptr+2;
     // cout<<a;
-    cout<<sizeof(ptr);
+    // cout<<sizeof(ptr);
+    // -----------------------------------------
+    int arr[4] = {10,20,30,40};
+    int* p = arr;
+    cout<<*(p)++;
+    // cout<<*(arr+3);
+    // cout<<arr<<endl; // Base Address
+    // cout<<&arr<<endl; // Base Address
+    // cout<<&arr[0]<<endl; // Base Address
+    // ------------------------------
+    // cout<<arr[1]<<endl; // 20
+    // cout<<*arr<<endl; // first index value
+    // cout<<*arr + 1 <<endl;// base address per jo value hai usme 1 add ker dea
+    // cout<<*(arr + 2); // yai third index ko denote kerage 
+    // ------------------------------
+    
 
     return 0;
 }
