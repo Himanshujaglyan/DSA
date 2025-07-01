5c3db50ed088648c754d671ab5fe0ee78cd7081e
Himanshujaglyan <114854175+Himanshujaglyan@users.noreply.github.com>
2025-07-01 22:37:07 +0530

    Lecture-3 (5 Qus Done)

--- DSA/MyLLProgram/LL_class3.cpp
+++ DSA/MyLLProgram/LL_class3.cpp
@@ -10,6 +10,13 @@ class Node{
         this->next = NULL;
     }
 };
+void PrintLL(Node* &head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+}
 int FindLen(Node* &head){
     Node* temp = head;
     int len = 0;
@@ -52,6 +59,56 @@ Node* FindMiddleNode2(Node* &head){
         }
     }
     return slow;}
+//! 3) Reverse Linked List in Group of K?
+Node* ReverseLLInK(Node* &head , int k){
+    int LLlen = FindLen(head); 
+    if(k > LLlen){
+        cout<<"Enter valid value of k"<<endl;
+        return head;
+    }
+    Node* prev = NULL;
+    Node* curr = head;
+    Node* nextNode = curr->next;    
+    int count = 0;
+    //pahle group ke according hamne khud reverse ker dea  
+    while(count < k){
+        nextNode = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = nextNode;
+        count++;
+    }
+    //baki ka kaam recursion sambhalega
+    if(nextNode != NULL){
+        head->next = ReverseLLInK(nextNode , k);
+    }
+return prev;
+}    
+//! 4) Check Linked list is circular or not (1)
+bool IscircularLL(Node* &head){
+    if(head == NULL) return false;
+    Node* temp = head->next;
+    while(temp!= NULL && temp != head){
+        temp = temp->next;    
+    }
+    return (temp == head);
+}
+//! 5) Check Linked list is circular or not (2)
+bool IsCircularLL2(Node* &head){
+    Node* fast = head;
+    Node* slow = head;
+    while(fast != NULL){
+        fast = fast->next;
+        if(fast != NULL){
+            fast = fast->next;
+            slow = slow->next;
+        }
+        if(fast == slow){
+            return true;
+        }
+    }
+    return false;
+}
 
 int main(){
     Node* head = new Node(10);
@@ -59,13 +116,15 @@ int main(){
     Node* third = new Node(30);
     Node* fourth = new Node(40);
     Node* fifth = new Node(50);
-    Node* tail = new Node(0);
+    Node* tail = new Node(60);
 
     head->next = second;
     second->next = third;
     third->next = fourth;
     fourth->next = fifth;
     fifth->next = tail;
+    //just for to make circular linked list
+    tail->next = head;
 
     //! 1) Find Middle Node in Linked List (using Length Approach)
     // Node* middleNode = FindMiddleNode1(head);
@@ -74,6 +133,20 @@ int main(){
     // Node* middleNode = FindMiddleNode2(head);
     // cout<<"Middle Node is : "<<middleNode->data<<endl;
     //! 3) Reverse Linked List in Group of K?
-    
+        //head = ReverseLLInK(head , 3);//means her third node se reverse karenge or jo baki ka kaam hoga vo recursion se karenge
+        // PrintLL(head);
+    //! 4) Is Linked List is circular or Not (First Approach)
+    // if(IscircularLL(head)){
+    //     cout<<"Yes it is circular LL"<<endl;
+    // }else{
+        //     cout<<"No it is not circular LL"<<endl;
+        // }
+    //! 5) Is Linked list is circular or Not (using Fast and Slow Approach)
+        // if(IsCircularLL2(head)){
+        //     cout<<"Yes it is circular LL"<<endl;
+        // }else{
+        //     cout<<"No it is not circular LL"<<endl;
+        // }
+    //! 6) 
     return 0;
 }
